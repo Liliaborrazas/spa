611e3f9e25d8a49335d3712fd6a18818f8b2af92
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-08-31 11:20:00 +0200

    feat: first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Routes
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+
+
+
+//Servicios
+
+
+
+//Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+    
   ],
   imports: [
-    BrowserModule
+  
+  BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
