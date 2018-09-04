832762820a8a3e7f561d32628bdab253dd1763fb
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-09-04 08:57:13 +0200

    feat:show a hero

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe:any ={};
 
-  ngOnInit() {
-  }
+  constructor( private activatedRouter: ActivatedRoute,
+              private _heroesService: HeroesService
+            ) {
+
+    this.activatedRouter.params.subscribe(params =>{
+      // console.log(params['id']);
+      this.heroe = this._heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
+    })
+   }
+
+  
 
 }
