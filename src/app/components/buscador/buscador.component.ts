302707741a50760b160e5f4a667d55cc92c1a150
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-09-10 18:05:59 +0200

    feat:create component buscador

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-buscador',
-  templateUrl: './buscador.component.html',
-  styles: []
+  templateUrl: './buscador.component.html'
+  
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute) {
+
+   }
 
   ngOnInit() {
+
+    this.activatedRoute.params.subscribe( params =>{
+      console.log( params['termino']);
+    } )
   }
 
 }
